6720f97ca7e45f328c1175c6d0641acdbe4952db
BHAVYA BANSAL <117255729+BhavyaBansal1@users.noreply.github.com>
2026-04-18 00:12:48 +0530

    Add files via upload

--- src123/app/app.config.ts
+++ src123/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
+
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
